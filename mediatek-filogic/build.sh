c9142c568feed398c11270cfc3d36d46c1a0e68c
cnstudiocom <nsrzsean@hotmail.com>
2025-01-18 16:37:05 +0800

    Update build.sh

--- mediatek-filogic/build.sh
+++ mediatek-filogic/build.sh
@@ -40,6 +40,10 @@ PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 
+PACKAGES="$PACKAGES netdata"
+PACKAGES="$PACKAGES kms-server"
+PACKAGES="$PACKAGES luci-app-smartdns"
+PACKAGES="$PACKAGES luci-app-ssr-plus"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
